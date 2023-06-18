e223c490e7cab47626c275f80b23e25f1effe050
Abhay Kumar <abhaykumar@Administrators-MacBook-Pro.local>
2023-06-18 18:14:03 +0530

    Loader style changes

--- src/app/core/models/enemy.model.ts
+++ src/app/core/models/enemy.model.ts
@@ -1,9 +1,31 @@
-export interface People {
+export interface Enemy {
     name: string,
-    homeworld: string,
+    id: number,
 }
 
-export interface PotentailEnemy{
-    names:string[],
-    volume:number
+export interface EnemyObject {
+    [key: string]: Enemy[]
+  }
+
+export class People {
+    name: string;
+    id: number;
+    homeworld: string;
+
+    getPeopleId(url:string):number {
+        let arr = url.split("/");
+        return +(arr.at(-2)!);
+    }
+
+    constructor(payload: any) {
+        this.name = payload.name;
+        this.id = this.getPeopleId(payload.url);
+        this.homeworld = payload.homeworld;
+    }
+}
+
+
+export interface PotentailEnemy {
+    names: string[],
+    volume: number
 }
--- src/app/modules/potential-rebel-enemies/potential-rebel-enemies.component.ts
+++ src/app/modules/potential-rebel-enemies/potential-rebel-enemies.component.ts
@@ -1,9 +1,9 @@
 import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, forkJoin } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, forkJoin } from 'rxjs';
 import { AppConstants } from 'src/app/app.constants';
-import { People, PotentailEnemy } from 'src/app/core/models/enemy.model';
+import { Enemy, EnemyObject, People, PotentailEnemy } from 'src/app/core/models/enemy.model';
 import { DataService } from 'src/app/core/services/data.service';
 import { HttpService } from 'src/app/core/services/http.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
@@ -19,8 +19,8 @@ export class PotentialRebelEnemiesComponent implements OnInit {
 
   searchText: FormControl = new FormControl('');
   minSearchTextLength = 2;
-  filteredPeople!: People[];
-  selectedPeople: any = {};
+  filteredPeople$: People[] = [];
+  selectedEnemy: EnemyObject = {} as EnemyObject;
   private BASE_URL = environment.BASE_URL;
   totalVolume = 0;
   objectkeys = Object.keys;
@@ -35,9 +35,15 @@ export class PotentialRebelEnemiesComponent implements OnInit {
   ngOnInit(): void {
     this.searchText.valueChanges.pipe(
       filter((value: string) => (value.length >= this.minSearchTextLength)),
-      debounceTime(500)).subscribe(query => {
+      debounceTime(500),
+      distinctUntilChanged()
+      ).subscribe(query => {
         this.searchPeople(query);
       })
+  }
+
+  public displayNull() {
+    return ''
   }
 
   searchPeople(query: string) {
@@ -45,45 +51,62 @@ export class PotentialRebelEnemiesComponent implements OnInit {
     let params = new HttpParams().set("search", query);
     this.httpServ.get(`${this.BASE_URL}${AppConstants.API_END_POINT.PEOPLE}`, params).subscribe(resp => {
       this.loaderServ.hide();
-      this.filteredPeople = resp.body.results;
+      this.filteredPeople$ = resp.body.results.map((data: any) => new People(data));
+      // console.log("this.filteredPeople$", this.filteredPeople$);
+
     })
   }
 
-  selectPeople(option: People) {
+  selectEnemy(option: People) {
     this.resetSelection();
-    this.selectedPeople[option.homeworld] = option.name;
+    if (this.selectedEnemy[option.homeworld]) {
+      if (this.isEnemyPresentInSamePlanet(option.id, this.selectedEnemy[option.homeworld])) {
+        alert(option.name + " is present in Enemy list")
+        return;
+      }
+      this.selectedEnemy[option.homeworld].push({ name: option.name, id: option.id });
+    } else {
+      this.selectedEnemy[option.homeworld] = [];
+      this.selectedEnemy[option.homeworld].push({ name: option.name, id: option.id });
+    }
+    // console.log("selectedEnemy", this.selectedEnemy);
+  }
+
+  isEnemyPresentInSamePlanet(peopleId: number, selectedEnemy: Enemy[]) {
+    return selectedEnemy.some(enemy => enemy.id === peopleId);
   }
 
   resetSelection() {
     this.searchText.setValue("");
-    this.filteredPeople = [];
+    this.filteredPeople$ = [];
   }
 
-  calculateEnemy() {
+  calculateEnemiesVolume() {
     this.loaderServ.show();
     let sources = [];
-    for (let key in this.selectedPeople) {
+    for (let key in this.selectedEnemy) {
       sources.push(this.httpServ.get(key));
     }
     forkJoin(sources).subscribe((resp) => {
       console.log(resp);
       this.totalVolume = 0;
       resp.forEach((planet) => {
-        this.totalVolume += this.computeEachVolume(+planet.body?.diameter);
+        this.totalVolume += this.computePlanetVolume(+planet.body?.diameter);
       });
       this.loaderServ.hide();
-      // console.log("total volume", this.totalVolume);
     });
   }
 
-  computeEachVolume(diameter: number) {
+  computePlanetVolume(diameter: number) {
     return (4 / 3) * Math.PI * Math.pow((diameter / 2), 3);
   }
 
   get potentialEnemiesName() {
     let names = [];
-    for (let key in this.selectedPeople) {
-      names.push(this.selectedPeople[key]);
+    for (let key in this.selectedEnemy) {
+      for (let val of this.selectedEnemy[key]) {
+        names.push(val.name);
+      }
     }
     return names;
   }
